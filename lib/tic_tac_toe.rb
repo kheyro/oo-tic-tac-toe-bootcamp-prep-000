2545a2da2997936f2e6c4c15d0dae8ef37c8dbd6
Denis Atkesone <denis.atkesone@gmail.com>
2017-11-07 13:13:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,7 +63,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    index.between?(0, 8) && !position_taken?(@board, index)
+    index.between?(0, 8) && !position_taken?(index)
   end
 
   def display_board
